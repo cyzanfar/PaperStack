46b0e48883aaac48df415deab3c2d94ac170c042
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-06-28 18:31:38 -0400

    add image upload with paperclip

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.2.4'
+gem 'paperclip', github: 'thoughtbot/paperclip'
+
 group :development, :test do
      gem 'sqlite3'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   #required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
 
