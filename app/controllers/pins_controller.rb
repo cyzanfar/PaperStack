b92c70f542e027573278cf2b0ee4890b2f2f0be2
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-06-29 20:16:21 -0400

    updated pin params in pins controller file

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -52,6 +52,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image, :file_avatar)
+      params.require(:pin).permit(:description, :image)
     end
 end
