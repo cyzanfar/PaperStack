92967ed5ae7c24366f42a4d9a0a6be8d559f6c51
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-07-01 02:21:03 -0400

    added aws config to pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,10 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-end
+	:storage => :s3,
+            :bucket  => ENV['S3_BUCKET_NAME']
+    end
+    
 
 
 
