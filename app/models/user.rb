ee2d77de9283686d8a4e65e16ebbdb62b3e5d3e1
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-07-07 18:51:14 -0500

    added validations name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :pins
+  
+  validates :name, presence: true
+
 end
