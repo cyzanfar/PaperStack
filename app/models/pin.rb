628fd143f194cffe2b6c3af65875ef5f85fd3bf2
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-07-07 19:03:38 -0500

    added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,8 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 	
+	validates :image, presence: true
+
     end
 
 
