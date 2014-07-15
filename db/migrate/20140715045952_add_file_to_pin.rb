ae17e10e4e30657927b4099fc80045663523f2fd
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-07-15 01:21:46 -0400

    added pdf and doc upload

---
+++ db/migrate/20140715045952_add_file_to_pin.rb
@@ -0,0 +1,12 @@
+class AddFileToPin < ActiveRecord::Migration
+ def self.up
+    change_table :pins do |t|
+      t.attachment :document
+   
+    end
+  end
+
+  	def self.down
+    	drop_attached_file :pins, :document
+	end
+  end
