1091a21fbd1e8a75858034ea3c1bae2f370cf6d4
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-07-15 01:26:25 -0400

    added pdf and doc upload

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,10 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-	
 	validates :image, presence: true
+
+	has_attached_file :document
+	validates_attachment :document, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "pins#index"
   get 'about' => 'pages#about'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
