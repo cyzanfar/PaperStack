6d47a42313941414849d3b6d9050f7eeb3a7a3f0
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-07-16 19:55:48 -0400

    customize about and home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,4 @@ class PagesController < ApplicationController
   def about
   end
 
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :pins
 
+ 
+
   devise_for :users
   root "pins#index"
   get 'about' => 'pages#about'
-  
+ 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
