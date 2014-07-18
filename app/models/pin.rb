f46693db032b4d1a27c6580a87db2631e620340c
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-07-17 20:13:25 -0400

    added validates to pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,7 +6,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :document
 	validates_attachment :document, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
-
+	validates :document , presence: true
     end
 
 
