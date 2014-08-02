ac30db0d874cd9f3b10c13882a9f68599bde4ea8
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2014-08-02 00:26:09 -0400

    initialize username for comment

--- config/initializers/commontator.rb
+++ config/initializers/commontator.rb
@@ -35,7 +35,7 @@ Commontator.configure do |config|
   # Arguments: a user (acts_as_commontator)
   # Returns: the user's name (String)
   # Default: lambda { |user| I18n.t('commontator.anonymous') } (all users are anonymous)
-  config.user_name_proc = lambda { |user| I18n.t('commontator.anonymous') }
+  config.user_name_proc = lambda { |user| user.name }
 
   # user_link_proc
   # Type: Proc
